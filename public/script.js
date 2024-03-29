fa325c9b68e8b6e7945d9d8c2e7a871e3449c8ef
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-03-29 22:11:04 +0100

    split client

---
+++ public/script.js
@@ -0,0 +1,67 @@
+const body = document.querySelector('body');
+const splash = document.querySelector('.splash');
+
+function getShot() {
+    body.classList.add('was-killed')
+}
+
+splash.addEventListener('transitionend', function () {
+    console.log("transitionend")
+    body.classList.remove('was-killed')
+})
+
+const connection = new WebSocket('ws://vinzenz-lpt2:8001');
+connection.binaryType = "blob"
+
+const controls = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+    32: "space",
+    87: "up", // 'w'
+    65: "left", // 'a'
+    83: "down", // 's'
+    68: "right", // 'd'
+};
+
+function start() {
+    window.onkeyup = function (event) {
+        if (controls[event.keyCode]) {
+            connection.send(JSON.stringify({
+                type: 'input-off',
+                value: controls[event.keyCode]
+            }))
+        }
+    }
+    window.onkeydown = function (event) {
+        if (controls[event.keyCode]) {
+            connection.send(JSON.stringify({
+                type: 'input-on',
+                value: controls[event.keyCode]
+            }))
+        }
+    }
+}
+
+connection.onmessage = function (message) {
+    message = JSON.parse(message.data)
+
+    console.log(message)
+
+    if (message.type === 'shot')
+        getShot()
+}
+
+connection.onopen = function () {
+    let name = "";
+
+    while (!name)
+        name = prompt("Player Name")
+
+    connection.send(JSON.stringify({
+        type: 'name',
+        value: name
+    }))
+    start()
+}
